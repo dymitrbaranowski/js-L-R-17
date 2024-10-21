32845dfae535d96613c0cf5049f065ce97b22a1d
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-10-21 21:55:05 +0300

    fox2

--- src/script.js
+++ src/script.js
@@ -3,21 +3,57 @@
 // https://uk.wikipedia.org/wiki/%D0%A7%D0%B0%D1%81_Unix
 
 // console.log('start');
-// setTimeout(()=>{console.log('setTimeout');},0)
-// console.log('finish');
-// console.time('test async')
-
-// console.log("start");
 
 // setTimeout(() => {
-//   console.log("setTimeout");
+//   console.log('setTimeout', 2);
 // }, 0);
 
-// for (let i = 0; i < 100000; i += 1) {
-//     console.log('value', i);
+// console.time('for');
+// for (let i = 0; i < 99999; i += 1) {
+//   console.log(i, 'for');
 // }
+// console.timeEnd('for');
 
-// console.timeEnd('test async')
+// console.log('end');
+
+// let counter = 0;
+// const id = setInterval(() => {
+//   counter += 1;
+//   console.log('hello world', counter);
+// }, 1000);
+// const id1 = setInterval(() => {
+//   counter += 1;
+//   //console.log('hello world', counter);
+// }, 1000);
+// const id2 = setInterval(() => {
+//   counter += 1;
+//   //console.log('hello world', counter);
+// }, 1000);
+// const id3 = setInterval(() => {
+//   counter += 1;
+//   //console.log('hello world', counter);
+// }, 1000);
+
+// console.log(id);
+// console.log(id1);
+// console.log(id2);
+// console.log(id3);
+// clearInterval(id);
+
+const content = document.querySelector('.content');
+const text = document.querySelector('.text');
+let counter = 8;
+text.textContent = `Залишилось ${counter} секунд`;
+
+const id = setInterval(() => {
+  counter -= 1;
+  text.textContent = `Залишилось ${counter} секунд`;
+}, 1000);
+
+setTimeout(() => {
+  clearInterval(id);
+  content.style.display = 'none';
+}, 1000 * counter);
 
 // const date = new Date();
 // console.log(date);
@@ -58,77 +94,76 @@
 //   box.style.display = "none";
 // }
 
-const day = document.querySelector(".date-day");
-const date = document.querySelector(".date");
-const month = document.querySelector(".date-month");
-const year = document.querySelector(".date-year");
-const digitalClock = document.querySelector(".digital-clock");
-const arrowSecond = document.querySelector(".clock-seconds__arrow");
-const arrowMinutes = document.querySelector(".clock-minutes__arrow");
-const arrowHours = document.querySelector(".clock-hours__arrow");
+// const day = document.querySelector(".date-day");
+// const date = document.querySelector(".date");
+// const month = document.querySelector(".date-month");
+// const year = document.querySelector(".date-year");
+// const digitalClock = document.querySelector(".digital-clock");
+// const arrowSecond = document.querySelector(".clock-seconds__arrow");
+// const arrowMinutes = document.querySelector(".clock-minutes__arrow");
+// const arrowHours = document.querySelector(".clock-hours__arrow");
 
-const namesOfMonth = [
-  "Січень",
-  "Лютий",
-  "Березень",
-  "Квітень",
-  "Травень",
-  "Червень",
-  "Липень",
-  "Серпень",
-  "Вересень",
-  "Жовтень",
-  "Листопад",
-  "Грудень",
-];
+// const namesOfMonth = [
+//   "Січень",
+//   "Лютий",
+//   "Березень",
+//   "Квітень",
+//   "Травень",
+//   "Червень",
+//   "Липень",
+//   "Серпень",
+//   "Вересень",
+//   "Жовтень",
+//   "Листопад",
+//   "Грудень",
+// ];
 
-const arrDay = [
-  "Неділя",
-  "Понеділок",
-  "Вівторок",
-  "Середа",
-  "Четвер",
-  "П`ятниця",
-  "Субота",
-];
+// const arrDay = [
+//   "Неділя",
+//   "Понеділок",
+//   "Вівторок",
+//   "Середа",
+//   "Четвер",
+//   "П`ятниця",
+//   "Субота",
+// ];
 
-setInterval(() => {
-  const currentTime = new Date();
-  const currentDay = arrDay[currentTime.getDay()];
-  const currentDate = currentTime.getDate();
-  const currentMonth = namesOfMonth[currentTime.getMonth()];
-  const currentYear = currentTime.getFullYear();
-  const currentHour = currentTime.getHours();
-  const currentMinutes = currentTime.getMinutes();
-  const currentSeconds = currentTime.getSeconds();
+// setInterval(() => {
+//   const currentTime = new Date();
+//   const currentDay = arrDay[currentTime.getDay()];
+//   const currentDate = currentTime.getDate();
+//   const currentMonth = namesOfMonth[currentTime.getMonth()];
+//   const currentYear = currentTime.getFullYear();
+//   const currentHour = currentTime.getHours();
+//   const currentMinutes = currentTime.getMinutes();
+//   const currentSeconds = currentTime.getSeconds();
 
-  const changeSeconds = (360 / 60) * currentSeconds;
-  const changeMinutes = (360 / 60) * currentMinutes;
-  const changeHours =(360 / 12) * currentHour + (360 / 12 / 60) * currentMinutes;
+//   const changeSeconds = (360 / 60) * currentSeconds;
+//   const changeMinutes = (360 / 60) * currentMinutes;
+//   const changeHours =(360 / 12) * currentHour + (360 / 12 / 60) * currentMinutes;
 
-  console.log(changeSeconds);
-  const formatTime = `${currentHour
-    .toString()
-    .padStart(2, "0")} : ${currentMinutes
-    .toString()
-    .padStart(2, "0")} : ${currentSeconds.toString().padStart(2, "0")}`;
+//   console.log(changeSeconds);
+//   const formatTime = `${currentHour
+//     .toString()
+//     .padStart(2, "0")} : ${currentMinutes
+//     .toString()
+//     .padStart(2, "0")} : ${currentSeconds.toString().padStart(2, "0")}`;
 
-  day.textContent = currentDay;
-  date.textContent = currentDate;
-  month.textContent = currentMonth;
-  year.textContent = currentYear;
+//   day.textContent = currentDay;
+//   date.textContent = currentDate;
+//   month.textContent = currentMonth;
+//   year.textContent = currentYear;
 
-  digitalClock.textContent = `Поточний час: ${formatTime}`;
+//   digitalClock.textContent = `Поточний час: ${formatTime}`;
 
-  arrowSecond.style.transform = `rotate(${changeSeconds}deg)`;
-  arrowMinutes.style.transform = `rotate(${changeMinutes}deg)`;
-  arrowHours.style.transform = `rotate(${changeHours}deg)`;
-}, 1000);
+//   arrowSecond.style.transform = `rotate(${changeSeconds}deg)`;
+//   arrowMinutes.style.transform = `rotate(${changeMinutes}deg)`;
+//   arrowHours.style.transform = `rotate(${changeHours}deg)`;
+// }, 1000);
 
 // // console.log(currentYear);
 
 // const currentTime = new Date();
-
 
 // const targetDate = new Date('4/11/2023')
 
@@ -138,14 +173,13 @@ setInterval(() => {
 //     console.log(convertMs(targetDate - currentDate));
 // },1000)
 
-
 // function convertMs(ms) {
 //     // Number of milliseconds per unit of time
 //     const second = 1000;
 //     const minute = second * 60;
 //     const hour = minute * 60;
 //     const day = hour * 24;
-  
+
 //     // Remaining days
 //     const days = Math.floor(ms / day);
 //     // Remaining hours
@@ -154,6 +188,6 @@ setInterval(() => {
 //     const minutes = Math.floor(((ms % day) % hour) / minute);
 //     // Remaining seconds
 //     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
+
 //     return { days, hours, minutes, seconds };
 //   }
