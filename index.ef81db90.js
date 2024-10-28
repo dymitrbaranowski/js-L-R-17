9df41865ff370c0de615e9e595eaab5cadd89ee7
dymitrbaranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-10-28 17:37:27 +0000

    Deploying to gh-pages from @ dymitrbaranowski/js-L-R-17@11f90047423b29786202953a13537aa857810a01 🚀

---
+++ index.ef81db90.js
@@ -0,0 +1,2 @@
+var promise=new Promise((function(o,n){setTimeout((function(){Math.random()>.2?o("Yeeees"):n("Noooo")}),3e3)}));console.log(promise),promise.then((function(o){})).catch((function(o){})).finally((function(){}));
+//# sourceMappingURL=index.ef81db90.js.map
