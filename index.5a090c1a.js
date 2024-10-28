bc04739eba67ad662bc84f571e6fe5000f796970
dymitrbaranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-10-28 16:56:21 +0000

    Deploying to gh-pages from @ dymitrbaranowski/js-L-R-17@a4e036ce9f7d270e9e499b3a3ac7738a0d1686b7 🚀

--- index.5a090c1a.js
+++
@@ -1,2 +0,0 @@
-var content=document.querySelector(".content"),text=document.querySelector(".text"),counter=8;text.textContent="Залишилось ".concat(counter," секунд");var id=setInterval((function(){counter-=1,text.textContent="Залишилось ".concat(counter," секунд")}),1e3);setTimeout((function(){clearInterval(id),content.style.display="none"}),1e3*counter);
-//# sourceMappingURL=index.5a090c1a.js.map
