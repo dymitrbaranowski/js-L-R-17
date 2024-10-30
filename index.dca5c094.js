68b38c5733cfaf4a2fad1160b58b1ce4cfcb5959
dymitrbaranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-10-30 06:39:51 +0000

    Deploying to gh-pages from @ dymitrbaranowski/js-L-R-17@f90da4129e7c0d78082ae7bedc523b1a76645924 🚀

---
+++ index.dca5c094.js
@@ -0,0 +1,2 @@
+var start=document.querySelector(".js-start"),container=document.querySelector(".js-container");function onStart(){}start.addEventListener("click",onStart);
+//# sourceMappingURL=index.dca5c094.js.map
